61fe3f16feb78f3da2badd22764ac7a3d9a8bcfd
Corey Dennison <cmgdennison@gmail.com>
2022-11-10 10:14:33 +0000

    SoftwareTools_BankingSys copy
    Created copy of SoftwareTools_BankingSys repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Software Tools - Bank System
+
+### Made by Jack Patterson, Hyden Johnson, Joe Diggins and Corey Dennison.
+Basic bank system app for our Software Tools module. 
---
+++ SimpleBankApp/SimpleBankApp/Account.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBankApp
+{
+    public class Account
+    {
+
+        int AccountId;
+        string Name;
+        string Iban;
+        int Pac;
+        float Balance; 
+ 
+        public Account(int AccountId, string Name, string Iban, int Pac, float Balance)
+        {
+            this.AccountId = AccountId;
+            this.Name = Name;
+            this.Iban = Iban;
+            this.Pac = Pac;
+            this.Balance = Balance;
+        }
+
+        public int getAccountId()
+        {
+            return AccountId;
+        }
+
+        public string getAccountName()
+        {
+            return Name;
+        }
+
+        public string getIban()
+        {
+            return Iban;
+        }
+
+        public int getPac()
+        {
+            return Pac;
+        }
+
+        public float getBalance()
+        {
+            return Balance;
+        }
+
+        public void setaccountId(int customerAccountId)
+        {
+            AccountId = customerAccountId;
+        }
+
+        public void setAccountName(string customerAccountName)
+        {
+            Name = customerAccountName;
+        }
+
+        public void setIban(string customerIban)
+        {
+            Iban = customerIban;
+        }
+
+        public void setPac(int customerPac)
+        {
+            Pac = customerPac;
+        }
+
+        public void setBalance(float customerBalance)
+        {
+            Balance = customerBalance;
+        }
+
+    }
+}
